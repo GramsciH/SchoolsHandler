58403186962ee94ca1cea9fa5322200cbfe29908
GramsciH <gramsciHC@gmail.com>
2013-09-25 23:01:48 -0400

    first commit

---
+++ schoolsHandler/index.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+	<title>Schools Handler</title>
+	<link rel="stylesheet" type="text/css" href="css/index.css">
+</head>
+
+<body>
+	<header>
+		<h1>
+			<img id="logo" src="images/logo.jpg">
+		</h1>
+	</header>
+
+	<nav id="nav_1">
+		<ul class="menu">
+			<li><a href="index.php">Inicio</a></li>
+			<li><a href="index.php">Administrativos</a></li>
+			<li><a href="index.php">Docentes</a></li>
+			<li><a href="index.php">Estudiantes</a></li>
+			<li><a href="index.php">Ayuda</a></li>
+		</ul>
+	</nav>
+
+	<article>
+		<section>
+			<p>Bienvenidos a Schools Handler la plataforma que administra plantel estudiantil, docente y administrativo de Unidades Educativas.</p>
+			<p>Toda la informacion que se maneja es solo disponible para los usuarios de la plataforma. La informacion que se otorgan son: Horarios, Planillas de Notas, Estadisticas (Indicadores) y todo tipo de información útil.</p>
+		</section>
+		<aside>
+			Aqui manejaremos los link's de los colegios que tengan una pagina Web.
+		</aside>
+	</article>
+
+	<footer>
+		<p>&copy;Copyrighted 2013 QueIdeasSoft - All Rights Reserved.</p>
+	</footer>
+</body>
+
+</html>
